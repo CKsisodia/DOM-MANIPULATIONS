187409365dd5fd56b019cfa1ab70e8b3f7fc994b
CKsisodia <cksisodia3@gmail.com>
2022-10-16 14:50:51 +0530

    task-7

--- task-4.js
+++ task-4.js
@@ -5,3 +5,50 @@ for(var i=0; i<items.length; i++){
     items[i].style.fontWeight = 'bold';
 }
 
+var itemlist = document.querySelector('#items');
+
+console.log(itemlist.parentNode);
+itemlist.parentElement.style.backgroundColor = 'green';
+
+console.log(itemlist.children);
+itemlist.children[1].style.backgroundColor='red';
+
+console.log(itemlist.firstChild);
+
+console.log(itemlist.firstElementChild);
+itemlist.firstElementChild.style.backgroundColor='yellow';
+
+console.log(itemlist.lastChild);
+
+console.log(itemlist.lastElementChild);
+itemlist.lastElementChild.style.backgroundColor='orange';
+
+console.log(itemlist.nextSibling);
+
+console.log(itemlist.nextElementSibling);
+
+console.log(itemlist.previousSibling);
+
+console.log(itemlist.previousElementSibling);
+itemlist.previousElementSibling.style.fontWeight='bold';
+
+
+var newDiv = document.createElement("div");
+newDiv.className = "hello";
+newDiv.id = "hello1";
+newDiv.setAttribute("title", "hello Div");
+var newDivText = document.createTextNode("Hello world");
+newDiv.appendChild(newDivText);
+
+console.log(newDiv);
+
+var container = document.querySelector("header .container");
+var h1 = document.querySelector("header h1");
+
+container.insertBefore(newDiv, h1);
+
+
+
+var h2 = document.getElementById("items");
+h2.innerHTML = 'Hello World' + h2.innerHTML;
+
