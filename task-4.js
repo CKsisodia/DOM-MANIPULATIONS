91e16fa7939faaa75df6a6bc4853b9680302e043
CKsisodia <cksisodia3@gmail.com>
2022-10-20 11:50:11 +0530

    task-11

--- task-4.js
+++ task-4.js
@@ -1,25 +1,28 @@
-var btn = document.querySelector(".btn");
-btn.addEventListener("click", (e) => {
-  document.querySelector("#my-form").style.background = "#90EE90";
-});
+// var btn = document.querySelector(".btn");
+// btn.addEventListener("click", (e) => {
+//   document.querySelector("#my-form").style.background = "#90EE90";
+// });
 
-var btn = document.querySelector(".btn");
-btn.addEventListener("mouseover", (e) => {
-  e.preventDefault();
-  document.querySelector("#my-form").style.background = "pink";
-});
+// var btn = document.querySelector(".btn");
+// btn.addEventListener("mouseover", (e) => {
+//   e.preventDefault();
+//   document.querySelector("#my-form").style.background = "pink";
+// });
 
-var btn = document.querySelector(".btn");
-btn.addEventListener("mouseout", (e) => {
-  e.preventDefault();
-  document.querySelector("#my-form").style.background = "orange";
-});
-function saveToLocalStorage(event){
-    event.preventDefault();
-    const Uname = event.target.username.value;
-    const Uemail = event.target.emailId.value;
+// var btn = document.querySelector(".btn");
+// btn.addEventListener("mouseout", (e) => {
+//   e.preventDefault();
+//   document.querySelector("#my-form").style.background = "orange";
+// });
 
-    localStorage.setItem('name',Uname);
-    localStorage.setItem('email',Uemail);
+function saveToLocalStorage(event) {
+  event.preventDefault();
+  const Uname = event.target.username.value;
+  const Uemail = event.target.emailId.value;
 
+  let UserDetail = {
+    Uname,
+    Uemail,
+  };
+  localStorage.setItem(UserDetail.Uemail,JSON.stringify(UserDetail));
 }
